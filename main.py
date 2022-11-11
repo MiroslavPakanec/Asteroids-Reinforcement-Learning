ed21909f33d59c77f19944418e3150322633cc68
miro <miropakanec@gmail.com>
2022-11-12 00:58:45 +0100

    proper asteroid trajectories

--- main.py
+++ main.py
@@ -89,6 +89,8 @@ class SpaceShip:
         self.boost_speec_factor = 0.36
         self.rotation_factor = 3
         self.projectiles: List[Projectile] = []
+        self.shooting_freq = 200
+        self.last_shooting_time = 0
 
     def set_image_path(self):
         if self.boost:
@@ -199,7 +201,9 @@ class SpaceShip:
 
     def set_shooting(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LCTRL]:
+        current_time = pygame.time.get_ticks()
+        if keys[pygame.K_LCTRL] and current_time > self.last_shooting_time + self.shooting_freq:
+            self.last_shooting_time = current_time
             pygame.draw.circle(self.screen, Colors.BLUE, self.position, 1)
             left_p, right_p = self.get_projectile_points(20, 17)
             projectile_left = Projectile(self.screen, left_p, self.rotation)
@@ -274,90 +278,98 @@ def build_space_ship(screen):
     )
 
 class Asteroid:
-    def __init__(self, screen, position, points, direction):
+    def __init__(self, screen, position, points, direction, level):
         self.screen = screen
         self.position = position
         self.original_position = position
         self.points = points # points of convex hull
         self.direction = direction
         self.rotation = random.uniform(0.1, 1) * math.pi / 180
-        self.speed = random.randint(1, 100) * 0.0001
-        self.hits = 0
-        self.durability = len(self.points) * 10
-
-    def hit(self):
-        self.hits += 1
-        if self.hits >= self.durability:
-            return True # destroy
-        return False
+        self.speed = random.randint(1, 100) * 0.001
+        self.level = level
 
     def rotate_point(self, center, point, angle):
         new_x = math.cos(angle) * (point[0]-center[0]) - math.sin(angle) * (point[1]-center[1]) + center[0]
         new_y = math.sin(angle) * (point[0]-center[0]) + math.cos(angle) * (point[1]-center[1]) + center[1]
         return (new_x, new_y)
 
+    def reset(self):
+        points = []
+        for p in self.points:
+            tx = p[0] - self.position[0]
+            ty = p[1] - self.position[1]
+            new_point = (self.original_position[0] + tx, self.original_position[1] + ty) # maintain rotation but respawn at origil pos 
+            points.append(new_point)
+            
+        self.points = points
+        self.position = self.original_position
+
     def get_trajectory_percentage(self):
         d_to_origin_x = self.position[0] - self.original_position[0]
         d_to_origin_y = self.position[1] - self.original_position[1]
-        d_to_target_x = self.position[0] - self.direction[0]
-        d_to_target_y = self.position[1] - self.direction[1]
+        d_total_x = self.original_position[0] - self.direction[0]
+        d_total_y = self.original_position[1] - self.direction[1]
         d_to_origin = math.sqrt((d_to_origin_x*d_to_origin_x)+(d_to_origin_y*d_to_origin_y))
-        d_to_target = math.sqrt((d_to_target_x*d_to_target_x)+(d_to_target_y*d_to_target_y))
-        d_total = d_to_origin + d_to_target
+        d_total = math.sqrt((d_total_x*d_total_x)+(d_total_y*d_total_y))
         return d_to_origin / d_total
 
     def step(self):
-        new_x = self.position[0] + self.direction[0] * self.speed
-        new_y = self.position[1] + self.direction[1] * self.speed
+        x_inc = (self.direction[0] - self.original_position[0]) * self.speed
+        y_inc = (self.direction[1] - self.original_position[1]) * self.speed
+
+        pygame.draw.line(self.screen, Colors.BLUE, self.position, self.direction)
+        pygame.draw.line(self.screen, Colors.RED, (0, 0), self.position)
+        pygame.draw.line(self.screen, Colors.GREEN, (0, 0), self.direction)
+        pygame.draw.circle(self.screen, Colors.RED, (self.position[0] + x_inc, self.position[1] + y_inc), 5)
+        print(self.get_trajectory_percentage())
+
+        new_x = self.position[0] + x_inc
+        new_y = self.position[1] + y_inc
         self.position = (new_x, new_y)
         new_points = []
         for p in self.points:
-            new_x = p[0]+self.direction[0]*self.speed
-            new_y = p[1]+self.direction[1]*self.speed
-            # new_rotated = self.rotate_point(center=self.position, point=(new_x, new_y), angle=self.rotation)
-            new_rotated = (new_x, new_y)
+            new_x = p[0]+x_inc
+            new_y = p[1]+y_inc
+            new_rotated = self.rotate_point(center=self.position, point=(new_x, new_y), angle=self.rotation)
             new_points.append(new_rotated)
         self.points = new_points
         return
 
     def render(self):
-        pygame.draw.line(screen, Colors.RED, self.original_position, self.direction, 1)
-        for i,_ in enumerate(self.points):
-            pygame.draw.line(screen, Colors.GREEN, self.points[i], self.position, 1)
-            if i == 0:
-                pygame.draw.line(screen, Colors.GREEN, self.points[i], self.points[len(self.points) - 1], 1)
-                continue
-            pygame.draw.line(screen, Colors.GREEN, self.points[i-1], self.points[i], 1)
-        pygame.draw.circle(screen, Colors.BLUE, self.position, 5)
+        pygame.draw.polygon(self.screen, Colors.GREEN, self.points)
+        # pygame.draw.line(screen, Colors.RED, self.original_position, self.direction, 1)
+
 
 class AsteroidBuilder:
     @staticmethod
     def generate_asteroid(screen):
-        size = random.randint(5, 100)
+        size = 50 #random.randint(5, 100)
         points, position, direction = np.array(AsteroidBuilder.generate_asteroid_points(size))
         hull = ConvexHull(points)
         corners = []
         for v in hull.vertices:
             p = points[v]
             corners.append((p[0], p[1]))
-        return Asteroid(screen, position, corners, direction)
+        return Asteroid(screen, position, corners, direction, level=size/10)
 
+
+    # @staticmethod
+    # generate_asteroid_point(size: int, )
 
     @staticmethod
     def generate_asteroid_points(size: int):
         n = size
-        spawn_d = math.sqrt(SCREEN_X*SCREEN_X + SCREEN_Y*SCREEN_Y) * 0.7 # spawn circle
-        direction_d = math.sqrt(SCREEN_X*SCREEN_X + SCREEN_Y*SCREEN_Y) * 0.7 # direction circle
+        spawn_dist = math.sqrt(SCREEN_X*SCREEN_X + SCREEN_Y*SCREEN_Y) * 0.2 # spawn / despawn circle
 
         spawn_alpha = random.randint(0, 359)
-        spawn_alpha_rad = spawn_alpha * math.pi / 180 # spawn angle rad
+        spawn_alpha_rad = spawn_alpha * math.pi / random.randint(140, 220) # spawn angle rad
         spawn_circle_center = (SCREEN_X / 2, SCREEN_Y / 2)
-        spawn_x = spawn_circle_center[0] + (spawn_d * math.cos(spawn_alpha_rad))
-        spawn_y = spawn_circle_center[1] + (spawn_d * math.sin(spawn_alpha_rad))
-        
+        spawn_x = spawn_circle_center[0] + (spawn_dist * math.cos(spawn_alpha_rad))
+        spawn_y = spawn_circle_center[1] + (spawn_dist * math.sin(spawn_alpha_rad))
+
         direction_alpha_rad = (spawn_alpha-random.randint(175, 185)) * math.pi / 180
-        direction_x = spawn_circle_center[0] + (direction_d * math.cos(direction_alpha_rad))
-        direction_y = spawn_circle_center[1] + (direction_d * math.sin(direction_alpha_rad))
+        direction_x = spawn_circle_center[0] + (spawn_dist * math.cos(direction_alpha_rad))
+        direction_y = spawn_circle_center[1] + (spawn_dist * math.sin(direction_alpha_rad))
 
         P = []
         orbits = np.random.normal(size/5, size, n)
@@ -373,35 +385,29 @@ class AsteroidController:
     def __init__(self, screen):
         self.screen = screen
         self.asteroids: List[Asteroid] = []
-        self.respawn_freq_ms = 200
+        self.respawn_freq_ms = 2000
         self.last_respawn_time = 0
         self.cleanup_count = 0
+        self.limit = 1
 
-    def step(self, curent_time):
+    def hit(self, asteroid_idx: int, asteroid: Asteroid):
+        self.asteroids.pop(asteroid_idx)
+        if asteroid.level == 1:
+            return
+        else: 
+            pass
+
+    def step(self):
+        current_time = pygame.time.get_ticks()
         for ai,asteroid in enumerate(self.asteroids):
-            can_be_removed = asteroid.step()
-            if can_be_removed:
-                self.asteroids.pop(ai)
+            asteroid.step()
+            if asteroid.get_trajectory_percentage() >= 1:
+                asteroid.reset()
         
-        if curent_time < self.last_respawn_time + self.respawn_freq_ms:
+        if current_time < self.last_respawn_time + self.respawn_freq_ms or len(self.asteroids) >= self.limit:
             return
         
-        # clean up
-        # for ai,asteroid in enumerate(self.asteroids):
-        #     trajectory_percent = asteroid.get_trajectory_percentage()
-        #     if trajectory_percent > 0.8:
-        #         self.asteroids.pop(ai)
-        #         self.cleanup_count += 1
-        # print(f'{len(self.asteroids)} {self.cleanup_count}')
-
-        # for i,asteroid in enumerate(self.asteroids):
-        #     print(f'{i} {asteroid.direction}')
-        # print('---')
-
-        if len(self.asteroids) > 5:
-            return
-
-        self.last_respawn_time = curent_time
+        self.last_respawn_time = current_time
         new_asteroid: Asteroid = AsteroidBuilder.generate_asteroid(self.screen)
         self.asteroids.append(new_asteroid)
 
@@ -414,16 +420,10 @@ class ColisionDetector:
     @staticmethod
     def asteroid_projectile(ac: AsteroidController, ship: SpaceShip):
         for ai,asteroid in enumerate(ac.asteroids):
-
             for pi, projectile in enumerate(ship.projectiles):
                 if ColisionDetector.is_inside_polygon(asteroid.points, projectile.position):
                     ship.projectiles.pop(pi)
-                    destroyed: bool = asteroid.hit()
-                    if destroyed:
-                        try:
-                            ac.asteroids.pop(ai)
-                        except:
-                            pass
+                    ac.hit(ai, asteroid)
     
     @staticmethod
     def is_inside_polygon(vertices, point):
@@ -455,7 +455,7 @@ def main(screen):
             screen.fill(Colors.BACKGROUND)
 
             ship.step()
-            asteroid_controller.step(pygame.time.get_ticks())
+            asteroid_controller.step()
 
             ColisionDetector.asteroid_projectile(asteroid_controller, ship)
 
